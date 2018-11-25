eb071ecf260e273792f27b40a7924c9d2c906787
Victor Wihlborg <jag@vbw.se>
2018-11-25 12:30:23 +0100

    Use fixture user

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,66 +1,84 @@
+import 'cypress-testing-library/add-commands';
+
 context('Login', () => {
     beforeEach(() => {
         cy.on('window:confirm', () => false);
     });
 
     it('can be achieved from home page', () => {
-        cy.visit('https://www.dev.zetkin.org/')
-            .getAllByText('Log in')
-            .click()
-            .get('input[type=email]')
-            .type('testadmin@example.com')
-            .get('input[type=password]')
-            .type('password')
-            .get('input[type=submit]')
-            .click()
-            .url()
-            .should('contain', '/dashboard');
+        cy.fixture('users/admin')
+            .as('user')
+            .then(user => {
+                cy.visit('https://www.dev.zetkin.org/')
+                    .getByText('Log in')
+                    .click()
+                    .get('input[type=email]')
+                    .type(user.email)
+                    .get('input[type=password]')
+                    .type(user.password)
+                    .get('input[type=submit]')
+                    .click()
+                    .url()
+                    .should('contain', '/dashboard');
+            });
     });
 
     it('works on mobile', () => {
-        cy.viewport('iphone-6')
-            .visit('https://www.dev.zetkin.org/')
-            .getAllByText('Log in')
-            .click()
-            .get('input[type=email]')
-            .type('testadmin@example.com')
-            .get('input[type=password]')
-            .type('password')
-            .get('input[type=submit]')
-            .click()
-            .url()
-            .should('contain', '/dashboard');
+        cy.fixture('users/admin')
+            .as('user')
+            .then(user => {
+                cy.viewport('iphone-6')
+                    .visit('https://www.dev.zetkin.org/')
+                    .getByText('Log in')
+                    .click()
+                    .get('input[type=email]')
+                    .type(user.email)
+                    .get('input[type=password]')
+                    .type(user.password)
+                    .get('input[type=submit]')
+                    .click()
+                    .url()
+                    .should('contain', '/dashboard');
+            });
     });
 
     it('shows error when missing email', () => {
-        cy.visit('https://www.dev.zetkin.org/')
-            .getAllByText('Log in')
-            .click()
-            .get('input[type=password]')
-            .type('password')
-            .get('input[type=submit]')
-            .click()
-            .get('.LoginForm-errorMessage')
-            .should('exist')
-            .contains(/all the fields/i);
+        cy.fixture('users/admin')
+            .as('user')
+            .then(user => {
+                cy.visit('https://www.dev.zetkin.org/')
+                    .getByText('Log in')
+                    .click()
+                    .get('input[type=password]')
+                    .type(user.password)
+                    .get('input[type=submit]')
+                    .click()
+                    .get('.LoginForm-errorMessage')
+                    .should('exist')
+                    .contains(/all the fields/i);
+            });
     });
 
     it('shows error when missing password', () => {
-        cy.visit('https://www.dev.zetkin.org/')
-            .getAllByText('Log in')
-            .click()
-            .get('input[type=email]')
-            .type('testadmin@example.com')
-            .get('input[type=submit]')
-            .click()
-            .get('.LoginForm-errorMessage')
-            .should('exist')
-            .contains(/all the fields/i);
+        cy.fixture('users/admin')
+            .as('user')
+            .then(user => {
+                cy.visit('https://www.dev.zetkin.org/')
+                    .getByText('Log in')
+                    .click()
+                    .get('input[type=email]')
+                    .type(user.email)
+                    .get('input[type=submit]')
+                    .click()
+                    .get('.LoginForm-errorMessage')
+                    .should('exist')
+                    .contains(/all the fields/i);
+            });
     });
 
     it('shows error for unknown user', () => {
         cy.visit('https://www.dev.zetkin.org/')
-            .getAllByText('Log in')
+            .getByText('Log in')
             .click()
             .get('input[type=email]')
             .type('testadmin2@example.com')
@@ -79,21 +97,25 @@ context('Login', () => {
     });
 
     it('shows error for invalid password', () => {
-        cy.visit('https://www.dev.zetkin.org/')
-            .getAllByText('Log in')
-            .click()
-            .get('input[type=email]')
-            .type('testadmin@example.com')
-            .get('input[type=password]')
-            .type('invalid')
-            .get('input[type=submit]')
-            .click()
-            .get('.LoginForm-errorMessage')
-            .should('exist')
-            .contains(/password/i)
-            .getAllByText(/forgot/i)
-            .click()
-            .url()
-            .should('contain', '/lost-password');
+        cy.fixture('users/admin')
+            .as('user')
+            .then(user => {
+                cy.visit('https://www.dev.zetkin.org/')
+                    .getByText('Log in')
+                    .click()
+                    .get('input[type=email]')
+                    .type(user.email)
+                    .get('input[type=password]')
+                    .type(`${user.password}invalid`)
+                    .get('input[type=submit]')
+                    .click()
+                    .get('.LoginForm-errorMessage')
+                    .should('exist')
+                    .contains(/password/i)
+                    .getByText(/forgot/i)
+                    .click()
+                    .url()
+                    .should('contain', '/lost-password');
+            });
     });
 });
